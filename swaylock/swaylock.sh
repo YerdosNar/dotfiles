d261d3556e7940191cdbdc8f9aa0ec61cd5159f3
YerdosNar <besway2000@gmail.com>
2025-12-28 07:40:57 +0900

    fixed swaylock

--- swaylock/swaylock.sh
+++ swaylock/swaylock.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-IMAGE=~/Pictures/arch_anime.jpg
-if [ $# -eq 1 ]; then
-    IMAGE=~/Pictures/$1
-fi
+IMAGE=./arch.png
 
 swaylock \
     --image $IMAGE \
@@ -16,6 +13,5 @@ swaylock \
     --indicator-radius 100 \
     --indicator-thickness 20 \
     --ring-color 0c79c3 \
-    # --fade-in 1 \
     --show-failed-attempts \
     --daemonize
